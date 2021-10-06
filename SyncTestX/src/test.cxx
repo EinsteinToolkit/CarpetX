88653d993a94a5d0dca9d04c53015f8c47be5e33
Samuel Cupp <sdcupp@gmail.com>
2021-10-06 12:12:46 -0500

    SyncTestX: Added tests for comparing sync time between Carpet and CarpetX
    Parameters added for weak scaling test for both Carpet and CarpetX.
    The meshes for Carpet are explicitly set to match the grids generated
    by CarpetX. Commented out IO is for setting up new grids if tests
    need to be changed.

--- SyncTestX/src/test.cxx
+++ SyncTestX/src/test.cxx
@@ -1,3 +1,4 @@
+#include <fixmath.hxx>
 #include <cctk.h>
 #include <cctk_Arguments.h>
 #include <cctk_Arguments_Checked.h>
@@ -19,17 +20,17 @@ extern "C" void SyncTestX_Initialize(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_SyncTestX_Initialize;
   DECLARE_CCTK_PARAMETERS;
 
-#  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
-#    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
-#    phi[index] = 0;
-#    t1[index] = 0;
-#    t2[index] = 0;
-#    t3[index] = 0;
-#    t4[index] = 0;
-#    t5[index] = 0;
-#  }
-#  CCTK_ENDLOOP3_ALL(SyncTest_Init);
-
+/*  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
+    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
+    phi[index] = 0;
+    t1[index] = 0;
+    t2[index] = 0;
+    t3[index] = 0;
+    t4[index] = 0;
+    t5[index] = 0;
+  }
+  CCTK_ENDLOOP3_ALL(SyncTest_Init);
+*/
   Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
     phi[p.idx] = 0;
     t1[p.idx] = 0;
@@ -52,18 +53,18 @@ extern "C" void SyncTestX_Initialize(CCTK_ARGUMENTS) {
 extern "C" void SyncTestX_Evolve(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_SyncTestX_Evolve;
   DECLARE_CCTK_PARAMETERS;
-
-#  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
-#    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
-#    phi[index] = 0;
-#    t1[index] = 0;
-#    t2[index] = 0;
-#    t3[index] = 0;
-#    t4[index] = 0;
-#    t5[index] = 0;
-#  }
-#  CCTK_ENDLOOP3_ALL(SyncTest_Init);
-
+/*
+  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
+    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
+    phi[index] = 0;
+    t1[index] = 0;
+    t2[index] = 0;
+    t3[index] = 0;
+    t4[index] = 0;
+    t5[index] = 0;
+  }
+  CCTK_ENDLOOP3_ALL(SyncTest_Init);
+*/
   Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
     phi[p.idx] = 0;
     t1[p.idx] = 0;
