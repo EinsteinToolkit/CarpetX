05d15e1a138fc6e5ee8bd45956d3f543673e370c
Roland Haas <rhaas@illinois.edu>
2021-12-26 11:23:57 -0600

    SyncTestX: allow single compilation for Carpet and CarpetX

--- SyncTestX/src/test.cxx
+++ SyncTestX/src/test.cxx
@@ -25,44 +25,40 @@ extern "C" void SyncTestX_Initialize(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_SyncTestX_Initialize;
   DECLARE_CCTK_PARAMETERS;
 
-// Can Carpet and CarpetX coexist in a single compilation?
-// If not, then the double if will be unnecessary. If so,
-// Then the code will eventually need to be changed to
-// allow for this.
 #ifdef HAVE_CAPABILITY_Carpet
-#ifdef HAVE_CAPABILITY_CarpetX
-#error "Can't have Carpet and CarpetX"
-#endif
-  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
-    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
-    phi[index] = 0;
-    t1[index] = 0;
-    t2[index] = 0;
-    t3[index] = 0;
-    t4[index] = 0;
-    t5[index] = 0;
+  if(CCTK_IsThornActive("Carpet")) {
+    CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
+      int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
+      phi[index] = 0;
+      t1[index] = 0;
+      t2[index] = 0;
+      t3[index] = 0;
+      t4[index] = 0;
+      t5[index] = 0;
+    } CCTK_ENDLOOP3_ALL(SyncTest_Init);
   }
-  CCTK_ENDLOOP3_ALL(SyncTest_Init);
 #endif
 
 #ifdef HAVE_CAPABILITY_CarpetX
-  Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
-    phi[p.idx] = 0;
-    t1[p.idx] = 0;
-    t2[p.idx] = 0;
-    t3[p.idx] = 0;
-    t4[p.idx] = 0;
-    t5[p.idx] = 0;
-  });
+  if(CCTK_IsThornActive("CarpetX")) {
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      phi[p.idx] = 0;
+      t1[p.idx] = 0;
+      t2[p.idx] = 0;
+      t3[p.idx] = 0;
+      t4[p.idx] = 0;
+      t5[p.idx] = 0;
+    });
 
-  Loop::loop_bnd<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
-    phi[p.idx] = 0;
-    t1[p.idx] = 0;
-    t2[p.idx] = 0;
-    t3[p.idx] = 0;
-    t4[p.idx] = 0;
-    t5[p.idx] = 0;
-  });
+    Loop::loop_bnd<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      phi[p.idx] = 0;
+      t1[p.idx] = 0;
+      t2[p.idx] = 0;
+      t3[p.idx] = 0;
+      t4[p.idx] = 0;
+      t5[p.idx] = 0;
+    });
+  }
 #endif
 }
 
@@ -71,39 +67,39 @@ extern "C" void SyncTestX_Evolve(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
 
 #ifdef HAVE_CAPABILITY_Carpet
-#ifdef HAVE_CAPABILITY_CarpetX
-#error "Can't have Carpet and CarpetX"
-#endif
-  CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
-    int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
-    phi[index] = 0;
-    t1[index] = 0;
-    t2[index] = 0;
-    t3[index] = 0;
-    t4[index] = 0;
-    t5[index] = 0;
+  if(CCTK_IsThornActive("Carpet")) {
+    CCTK_LOOP3_ALL(SyncTest_Init,cctkGH,i,j,k) {
+      int index = CCTK_GFINDEX3D(cctkGH,i,j,k);
+      phi[index] = 0;
+      t1[index] = 0;
+      t2[index] = 0;
+      t3[index] = 0;
+      t4[index] = 0;
+      t5[index] = 0;
+    } CCTK_ENDLOOP3_ALL(SyncTest_Init);
   }
-  CCTK_ENDLOOP3_ALL(SyncTest_Init);
 #endif
 
 #ifdef HAVE_CAPABILITY_CarpetX
-  Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
-    phi[p.idx] = 0;
-    t1[p.idx] = 0;
-    t2[p.idx] = 0;
-    t3[p.idx] = 0;
-    t4[p.idx] = 0;
-    t5[p.idx] = 0;
-  });
+  if(CCTK_IsThornActive("CarpetX")) {
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      phi[p.idx] = 0;
+      t1[p.idx] = 0;
+      t2[p.idx] = 0;
+      t3[p.idx] = 0;
+      t4[p.idx] = 0;
+      t5[p.idx] = 0;
+    });
 
-  Loop::loop_bnd<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
-    phi[p.idx] = 0;
-    t1[p.idx] = 0;
-    t2[p.idx] = 0;
-    t3[p.idx] = 0;
-    t4[p.idx] = 0;
-    t5[p.idx] = 0;
-  });
+    Loop::loop_bnd<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      phi[p.idx] = 0;
+      t1[p.idx] = 0;
+      t2[p.idx] = 0;
+      t3[p.idx] = 0;
+      t4[p.idx] = 0;
+      t5[p.idx] = 0;
+    });
+  }
 #endif
 }
 
