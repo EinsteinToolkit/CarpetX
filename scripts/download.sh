fd22e4f707651755dc11c9931e2b0b804f859a73
Erik Schnetter <schnetter@gmail.com>
2023-02-20 15:49:23 -0500

    CI: Disable

---
+++ scripts/download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ex
+
+export CARPETXSPACE="$PWD/CarpetX"
+export WORKSPACE="$PWD/workspace"
+export PAGESSPACE="$PWD/gh-pages"
+mkdir -p "$WORKSPACE"
+cd "$WORKSPACE"
+
+# Check out Cactus
+wget https://raw.githubusercontent.com/gridaphobe/CRL/master/GetComponents
+chmod a+x GetComponents
+./GetComponents --no-parallel --shallow https://bitbucket.org/einsteintoolkit/manifest/raw/master/einsteintoolkit.th
+
+# Download old CarpetX thorns for the external libraries there
+# TODO: Move these into ExternalLibraries
+pushd repos
+git clone https://bitbucket.org/eschnett/cactusamrex
+popd
+pushd arrangements/ExternalLibraries
+ln -s ../../repos/cactusamrex/ADIOS2 .
+ln -s ../../repos/cactusamrex/AMReX .
+ln -s ../../repos/cactusamrex/Boost .
+ln -s ../../repos/cactusamrex/NSIMD .
+ln -s ../../repos/cactusamrex/Silo .
+ln -s ../../repos/cactusamrex/openPMD_api .
+ln -s ../../repos/cactusamrex/yaml_cpp .
+popd
+
+# Create a link to the CarpetX repository
+ln -s "$CARPETXSPACE" repos
+# Create links for the CarpetX thorns
+mkdir -p arrangements/CarpetX
+pushd arrangements/CarpetX
+ln -s ../../repos/CarpetX/* .
+popd
+
+# Unshallow Carpetx
+pushd Cactus/repos/CarpetX
+git fetch --unshallow
+echo "At git ref: $(git rev-parse HEAD)"
+popd
