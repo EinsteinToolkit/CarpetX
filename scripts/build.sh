454f20b88717c7d68c0d240b396a95b66aed2ed4
Erik Schnetter <schnetter@gmail.com>
2023-02-20 20:17:34 -0500

    CI: Happify Formaline

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,9 +17,9 @@ cp "$CARPETXSPACE/scripts/actions.sub" ./simfactory/mdb/submitscripts
 cp "$CARPETXSPACE/scripts/carpetx.th" .
 cp "$CARPETXSPACE/scripts/defs.local.ini" ./simfactory/etc
 
-# # For Formaline
-# git config --global user.email "carpetx@einsteintoolkit.org"
-# git config --global user.name "Github Actions"
+# For Formaline
+git config --global user.email "carpetx@einsteintoolkit.org"
+git config --global user.name "Github Actions"
 
 # Build
 time ./simfactory/bin/sim --machine=actions build -j $(nproc) sim
