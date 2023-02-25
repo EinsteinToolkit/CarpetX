3c06483f367956462bc6d26b95bae967bac88f85
Erik Schnetter <schnetter@gmail.com>
2023-02-25 11:20:21 -0500

    CI: Simplify checking for failed tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -22,13 +22,22 @@ ONEPROC_DIR="$(./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECIS
 time ./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" create-run TestJob01_temp_2 --cores 2 --num-threads 1 --testsuite --select-tests=CarpetX
 TWOPROC_DIR="$(./simfactory/bin/sim --machine="actions-$ACCELERATOR-$REAL_PRECISION" get-output-dir TestJob01_temp_2)/TEST/sim"
 
-# Parse results and generate plots
-cd "$PAGESSPACE"
-python3 "$CARPETXSPACE/scripts/store.py" "$WORKSPACE/Cactus/repos/CarpetX" "$ONEPROC_DIR" "$TWOPROC_DIR"
-python3 "$CARPETXSPACE/scripts/logpage.py" "$WORKSPACE/Cactus/repos/CarpetX"
-
+# # Parse results and generate plots
+# cd "$PAGESSPACE"
+# python3 "$CARPETXSPACE/scripts/store.py" "$WORKSPACE/Cactus/repos/CarpetX" "$ONEPROC_DIR" "$TWOPROC_DIR"
+# python3 "$CARPETXSPACE/scripts/logpage.py" "$WORKSPACE/Cactus/repos/CarpetX"
+# 
 # # Store HTML results
 # git add docs
 # git add records
 # git add test_nums.csv
 # git commit -m "Add new test result" && git push
+
+TESTS_FAILED=False
+for test_dir in "${ONEPROC_DIR}" "${TWOPROC_DIR}"; do
+    log="${test_dir}/summary.log"
+    if !grep -q '^ Number failed            -> 0$' ${log}; then
+        TESTS_FAILED=True
+    fi
+done
+echo ${TESTS_FAILED} >>${GITHUB_ENV}
