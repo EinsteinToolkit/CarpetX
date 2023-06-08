e69f155ef3de05847c2d0b161ff909ad7005a608
Erik Schnetter <schnetter@gmail.com>
2023-06-08 12:08:52 -0400

    HydroBase: Add magnetic vector potential A^i

--- HydroBase/src/hydrobase.cxx
+++ HydroBase/src/hydrobase.cxx
@@ -27,6 +27,16 @@ extern "C" void HydroBase_initial_data(CCTK_ARGUMENTS) {
                                       Bvecy(p.I) = 0;
                                       Bvecz(p.I) = 0;
                                     });
+
+  grid.loop_all_device<1, 0, 0>(
+      grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p)
+                            CCTK_ATTRIBUTE_ALWAYS_INLINE { Avecx(p.I) = 0; });
+  grid.loop_all_device<0, 1, 0>(
+      grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p)
+                            CCTK_ATTRIBUTE_ALWAYS_INLINE { Avecy(p.I) = 0; });
+  grid.loop_all_device<0, 0, 1>(
+      grid.nghostzones, [=] CCTK_DEVICE(const PointDesc &p)
+                            CCTK_ATTRIBUTE_ALWAYS_INLINE { Avecz(p.I) = 0; });
 }
 
 } // namespace HydroBase
