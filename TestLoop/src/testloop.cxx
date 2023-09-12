df84fde842febbcb39b78cf317f2a6f54932494a
Cheng-Hsin Cheng <chcheng3@illinois.edu>
2023-09-12 12:46:00 -0500

    TestLoop: Rename loop_int_bnd as loop_outermost_int

--- TestLoop/src/testloop.cxx
+++ TestLoop/src/testloop.cxx
@@ -27,14 +27,14 @@ extern "C" void TestLoop_OutermostInterior(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTSX_TestLoop_OutermostInterior;
   DECLARE_CCTK_PARAMETERS;
 
-  grid.loop_int_bnd<0, 0, 0>(
+  grid.loop_outermost_int<0, 0, 0>(
       grid.nghostzones,
       [=] CCTK_DEVICE CCTK_HOST(const PointDesc &p)
           CCTK_ATTRIBUTE_ALWAYS_INLINE {
           testloop_gf(p.I) += 10.0;
       });
 
-  grid.loop_int_bnd_device<0, 0, 0>(
+  grid.loop_outermost_int_device<0, 0, 0>(
       grid.nghostzones,
       [=] CCTK_DEVICE CCTK_HOST(const PointDesc &p)
           CCTK_ATTRIBUTE_ALWAYS_INLINE {
