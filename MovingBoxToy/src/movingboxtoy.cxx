055902392349eeb7bfa66ac5d9eb2a57ef69cf33
lucass-carneiro <lucas.t.s.carnbeiro@gmail.com>
2023-10-24 10:56:11 -0500

    Removed RESTRICTED from BoxInBox. The MovingBoxToy thorn now moves boxes without requiring acces to BoxInBox parameters

--- MovingBoxToy/src/movingboxtoy.cxx
+++ MovingBoxToy/src/movingboxtoy.cxx
@@ -14,20 +14,19 @@ extern "C" void MovingBoxToy_MoveBoxes(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
 
   using std::cos;
+  using std::sqrt;
 
   const CCTK_REAL omega{M_PI/4};
 
-  // Initial positions of box 1
-  const auto x0_1{position_x_1};
-
-  // Initial positions of box 2
-  const auto x0_2{position_x_2};
+  // Radius of each box
+  const auto R1 = sqrt(pow2(position_x[0]) + pow2(position_y[0]) + pow2(position_z[0]));
+  const auto R2 = sqrt(pow2(position_x[1]) + pow2(position_y[1]) + pow2(position_z[1])); 
 
   // Trajectory of box 1
-  position_x[0] = x0_1 * cos(omega * cctk_time);
-  position_y[0] = x0_1 * sin(omega * cctk_time);
+  position_x[0] = R1 * cos(omega * cctk_time);
+  position_y[0] = R1 * sin(omega * cctk_time);
 
   // Trajectory of box 2
-  position_x[1] = x0_2 * cos(omega * cctk_time);
-  position_y[1] = x0_2 * sin(omega * cctk_time);
+  position_x[1] = -R2 * cos(omega * cctk_time);
+  position_y[1] = -R2 * sin(omega * cctk_time);
 }
