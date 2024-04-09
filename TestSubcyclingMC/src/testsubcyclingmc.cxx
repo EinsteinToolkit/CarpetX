06df6035bc68547419edcdc1047f7c1a6ede75d0
Liwei Ji <jiliwei.phys@gmail.com>
2024-04-09 18:21:00 +0000

    TestSubcyclingMC: fix bug in initial data

--- TestSubcyclingMC/src/testsubcyclingmc.cxx
+++ TestSubcyclingMC/src/testsubcyclingmc.cxx
@@ -18,27 +18,30 @@ using namespace Arith;
 constexpr int dim = 3;
 
 // u(t,x,y,z) =
-//   A cos(2 π omega t) cos(2 π kx x) cos(2 π ky y) cos(2 π kz z)
+//   A cos(2 pi omega t) sin(2 pi kx x) sin(2 pi ky y) sin(2 pi kz z)
 template <typename T>
-constexpr void standing_wave(const T A, const Arith::vect<T, dim> &k, const T t,
-                             const Arith::vect<T, dim> &x, T &u) {
+constexpr void standing_wave(const T A, const T kx, const T ky, const T kz,
+                             const T t, const T x, const T y, const T z, T &u,
+                             T &rho) {
   using std::acos, std::cos, std::pow, std::sin, std::sqrt;
 
   const T pi = acos(-T(1));
-  const T omega = sqrt(dot(k, k));
+  const T omega = sqrt(pow(kx, 2) + pow(ky, 2) + pow(kz, 2));
 
-  u = A * cos(2 * pi * omega * t) * cos(2 * pi * k[0] * x[0]) *
-      cos(2 * pi * k[1] * x[1]) * cos(2 * pi * k[2] * x[2]);
+  u = A * cos(2 * pi * omega * t) * cos(2 * pi * kx * x) *
+      cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
+  rho = A * (-2 * pi * omega) * sin(2 * pi * omega * t) * cos(2 * pi * kx * x) *
+        cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
 }
 
 // u(t,r) = (f(t-r) - f(t+r)) / r
 // f(v) = A exp(-1/2 (r/W)^2)
 template <typename T>
-constexpr void gaussian(const T A, const T W, const T t,
-                        const Arith::vect<T, dim> &x, T &u) {
+constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
+                        const T z, T &u, T &rho) {
   using std::exp, std::pow, std::sqrt;
 
-  const T r = sqrt(dot(x, x));
+  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 
   const auto f = [&](const T v) {
     return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
@@ -47,8 +50,10 @@ constexpr void gaussian(const T A, const T W, const T t,
   if (r < sqrt(std::numeric_limits<T>::epsilon())) {
     // L'Hôpital
     u = 2 / pow(W, 2) * f(t) * t;
+    rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
   } else {
     u = (f(t - r) - f(t + r)) / r;
+    rho = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
   }
 }
 
@@ -61,12 +66,9 @@ extern "C" void TestSubcyclingMC_Initial(CCTK_ARGUMENTS) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          CCTK_REAL u0;
-          standing_wave(amplitude,
-                        {standing_wave_kx, standing_wave_ky, standing_wave_kz},
-                        cctk_time, p.X, u0);
-          u(p.I) = u0;
-          rho(p.I) = 0;
+          standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
+                        standing_wave_kz, cctk_time, p.x, p.y, p.z, u(p.I),
+                        rho(p.I));
         });
 
   } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
@@ -74,10 +76,8 @@ extern "C" void TestSubcyclingMC_Initial(CCTK_ARGUMENTS) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          CCTK_REAL u0;
-          gaussian(amplitude, gaussian_width, cctk_time, p.X, u0);
-          u(p.I) = u0;
-          rho(p.I) = 0;
+          gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u(p.I),
+                   rho(p.I));
         });
 
   } else {
