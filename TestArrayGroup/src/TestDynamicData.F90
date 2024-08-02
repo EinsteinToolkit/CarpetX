a1848f57ed054d9ee07871bbc58bbde690b813e2
Roland Haas <rhaas@illinois.edu>
2024-08-02 14:58:49 -0500

    TestArrayGroup: add tests for integer and complex grid arrays

--- TestArrayGroup/src/TestDynamicData.F90
+++ TestArrayGroup/src/TestDynamicData.F90
@@ -28,4 +28,46 @@ subroutine TestArrayGroup_DynamicDataF(CCTK_ARGUMENTS)
       call CCTK_ERROR("incorrect size in test3 array dynamic data")
   endif
 
+  if(RANK(test_int1) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_int1 array dynamic data")
+  endif
+  if(SIZE(test_int1, 1) /= 5 .or. SIZE(test_int1, 2) /= 6 .or. SIZE(test_int1, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_int1 array dynamic data")
+  endif
+
+  if(RANK(test_int2) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_int2 array dynamic data")
+  endif
+  if(SIZE(test_int2, 1) /= 5 .or. SIZE(test_int2, 2) /= 6 .or. SIZE(test_int2, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_int2 array dynamic data")
+  endif
+
+  if(RANK(test_int3) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_int3 array dynamic data")
+  endif
+  if(SIZE(test_int3, 1) /= 5 .or. SIZE(test_int3, 2) /= 6 .or. SIZE(test_int3, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_int3 array dynamic data")
+  endif
+
+  if(RANK(test_complex1) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_complex1 array dynamic data")
+  endif
+  if(SIZE(test_complex1, 1) /= 5 .or. SIZE(test_complex1, 2) /= 6 .or. SIZE(test_complex1, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_complex1 array dynamic data")
+  endif
+
+  if(RANK(test_complex2) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_complex2 array dynamic data")
+  endif
+  if(SIZE(test_complex2, 1) /= 5 .or. SIZE(test_complex2, 2) /= 6 .or. SIZE(test_complex2, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_complex2 array dynamic data")
+  endif
+
+  if(RANK(test_complex3) /= 3) then ! note rank 3 b/c of vector of rank=2 arrays
+      call CCTK_ERROR("incorrect dimension in test_complex3 array dynamic data")
+  endif
+  if(SIZE(test_complex3, 1) /= 5 .or. SIZE(test_complex3, 2) /= 6 .or. SIZE(test_complex3, 3) /= 4) then
+      call CCTK_ERROR("incorrect size in test_complex3 array dynamic data")
+  endif
+
 end subroutine TestArrayGroup_DynamicDataF
