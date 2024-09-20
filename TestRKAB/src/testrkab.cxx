2586ba92ab00be6c746847212a4770b92e272605
Liwei Ji <jiliwei.phys@gmail.com>
2024-09-19 21:54:31 -0400

    TestRKAB: add thorn

---
+++ TestRKAB/src/testrkab.cxx
@@ -0,0 +1,143 @@
+#include <loop_device.hxx>
+
+#include <sum.hxx>
+#include <vect.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments.h>
+#include <cctk_Parameters.h>
+
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <limits>
+
+namespace TestRKAB {
+using namespace Arith;
+
+constexpr int dim = 3;
+
+// u(t,x,y,z) =
+//   A cos(2 pi omega t) sin(2 pi kx x) sin(2 pi ky y) sin(2 pi kz z)
+template <typename T>
+constexpr void standing_wave(const T A, const T kx, const T ky, const T kz,
+                             const T t, const T x, const T y, const T z, T &u,
+                             T &rho) {
+  using std::acos, std::cos, std::pow, std::sin, std::sqrt;
+
+  const T pi = acos(-T(1));
+  const T omega = sqrt(pow(kx, 2) + pow(ky, 2) + pow(kz, 2));
+
+  u = A * cos(2 * pi * omega * t) * cos(2 * pi * kx * x) *
+      cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
+  rho = A * (-2 * pi * omega) * sin(2 * pi * omega * t) * cos(2 * pi * kx * x) *
+        cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
+}
+
+// u(t,r) = (f(t-r) - f(t+r)) / r
+// f(v) = A exp(-1/2 (r/W)^2)
+template <typename T>
+constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
+                        const T z, T &u, T &rho) {
+  using std::exp, std::pow, std::sqrt;
+
+  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+  const auto f = [&](const T v) {
+    return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
+  };
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    // L'Hôpital
+    u = 2 / pow(W, 2) * f(t) * t;
+    rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
+  } else {
+    u = (f(t - r) - f(t + r)) / r;
+    rho = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
+  }
+}
+
+extern "C" void TestRKAB_Initial(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_TestRKAB_Initial;
+  DECLARE_CCTK_PARAMETERS;
+
+  if (CCTK_EQUALS(initial_condition, "standing wave")) {
+    grid.loop_int_device<0, 0, 0>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
+                        standing_wave_kz, cctk_time, p.x, p.y, p.z, u(p.I),
+                        rho(p.I));
+        });
+
+  } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
+    grid.loop_int_device<0, 0, 0>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u(p.I),
+                   rho(p.I));
+        });
+
+  } else {
+    CCTK_ERROR("Unknown initial condition");
+  }
+}
+
+extern "C" void TestRKAB_RHS(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_TestRKAB_RHS;
+  DECLARE_CCTK_PARAMETERS;
+
+  grid.loop_int_device<0, 0, 0>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        using std::pow;
+        Arith::vect<CCTK_REAL, dim> ddu;
+        for (int d = 0; d < dim; ++d) {
+          // ddu[d] = (u(p.I - p.DI[d]) - 2 * u(p.I) + u(p.I + p.DI[d])) /
+          //          pow(p.DX[d], 2);
+          ddu[d] = (-(u(p.I - 2 * p.DI[d]) + u(p.I + 2 * p.DI[d])) +
+                    16 * (u(p.I - p.DI[d]) + u(p.I + p.DI[d])) - 30 * u(p.I)) /
+                   (12 * pow(p.DX[d], 2));
+        }
+        u_rhs(p.I) = rho(p.I);
+        rho_rhs(p.I) = ddu[0] + ddu[1] + ddu[2];
+      });
+}
+
+extern "C" void TestRKAB_Sync(CCTK_ARGUMENTS) {
+  // do nothing
+}
+
+extern "C" void TestRKAB_Error(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTSX_TestRKAB_Error;
+  DECLARE_CCTK_PARAMETERS;
+
+  if (CCTK_EQUALS(initial_condition, "standing wave")) {
+
+    grid.loop_int_device<0, 0, 0>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          CCTK_REAL u0, rho0;
+          standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
+                        standing_wave_kz, cctk_time, p.x, p.y, p.z, u0, rho0);
+          u_err(p.I) = u(p.I) - u0;
+          rho_err(p.I) = rho(p.I) - rho0;
+        });
+
+  } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
+
+    grid.loop_int_device<0, 0, 0>(
+        grid.nghostzones,
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          CCTK_REAL u0, rho0;
+          gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u0,
+                   rho0);
+          u_err(p.I) = u(p.I) - u0;
+          rho_err(p.I) = rho(p.I) - rho0;
+        });
+
+  } else {
+    CCTK_ERROR("Unknown initial condition");
+  }
+}
+
+} // namespace TestRKAB
