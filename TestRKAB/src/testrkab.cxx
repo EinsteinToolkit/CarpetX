84b58ecde8bce738eb2cfb41a07555bd7202bf2c
Lucas <lucas.t.s.carneiro@gmail.com>
2024-10-03 13:48:58 -0500

    TestRKAB: Updated the thorn to use a first order formulation for the wave equation. Standing wave data available. Gaussian data pending

--- TestRKAB/src/testrkab.cxx
+++ TestRKAB/src/testrkab.cxx
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <iterator>
 #include <loop_device.hxx>
 
 #include <sum.hxx>
@@ -17,64 +19,69 @@ using namespace Arith;
 
 constexpr int dim = 3;
 
-// u(t,x,y,z) =
-//   A cos(2 pi omega t) sin(2 pi kx x) sin(2 pi ky y) sin(2 pi kz z)
+// Standing wave functions
 template <typename T>
-constexpr void standing_wave(const T A, const T kx, const T ky, const T kz,
-                             const T t, const T x, const T y, const T z, T &u,
-                             T &rho) {
-  using std::acos, std::cos, std::pow, std::sin, std::sqrt;
-
-  const T pi = acos(-T(1));
-  const T omega = sqrt(pow(kx, 2) + pow(ky, 2) + pow(kz, 2));
-
-  u = A * cos(2 * pi * omega * t) * cos(2 * pi * kx * x) *
-      cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
-  rho = A * (-2 * pi * omega) * sin(2 * pi * omega * t) * cos(2 * pi * kx * x) *
-        cos(2 * pi * ky * y) * cos(2 * pi * kz * z);
-}
-
-// u(t,r) = (f(t-r) - f(t+r)) / r
-// f(v) = A exp(-1/2 (r/W)^2)
-template <typename T>
-constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
-                        const T z, T &u, T &rho) {
-  using std::exp, std::pow, std::sqrt;
-
-  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-  const auto f = [&](const T v) {
-    return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
-  };
-
-  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
-    // L'Hôpital
-    u = 2 / pow(W, 2) * f(t) * t;
-    rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
-  } else {
-    u = (f(t - r) - f(t + r)) / r;
-    rho = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
-  }
+static inline auto sw_phi(T A, T kx, T ky, T kz, T t, T x, T y,
+                          T z) noexcept -> T {
+  using std::sqrt, std::sin, std::cos;
+  const auto pi{acos(T{-1})};
+  const auto omega{sqrt(kx * kx + ky * ky + kz * kz)};
+  return A * cos(2 * omega * pi * t) * sin(2 * kx * pi * x) *
+         sin(2 * ky * pi * y) * sin(2 * kz * pi * z);
 }
 
 template <typename T>
-constexpr void dtgaussian(const T A, const T W, const T t, const T x, const T y,
-                          const T z, T &dtu, T &dtrho) {
-  using std::exp, std::pow, std::sqrt;
+static inline auto sw_Pi(T A, T kx, T ky, T kz, T t, T x, T y,
+                         T z) noexcept -> T {
+  using std::sqrt, std::sin, std::cos;
+  const auto pi{acos(T{-1})};
+  const auto omega{sqrt(kx * kx + ky * ky + kz * kz)};
+  return -2 * A * omega * pi * sin(2 * omega * pi * t) * sin(2 * kx * pi * x) *
+         sin(2 * ky * pi * y) * sin(2 * kz * pi * z);
+}
 
-  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-  const auto f = [&](const T v) {
-    return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
-  };
+template <typename T>
+static inline auto sw_Dx(T A, T kx, T ky, T kz, T t, T x, T y,
+                         T z) noexcept -> T {
+  using std::sqrt, std::sin, std::cos;
+  const auto pi{acos(T{-1})};
+  const auto omega{sqrt(kx * kx + ky * ky + kz * kz)};
+  return 2 * A * kx * pi * cos(2 * omega * pi * t) * cos(2 * kx * pi * x) *
+         sin(2 * ky * pi * y) * sin(2 * kz * pi * z);
+}
 
-  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
-    dtu = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
-    dtrho = (2 * t * (pow(t, 2) - 3 * pow(W, 2)) * f(t)) / pow(W, 6);
-  } else {
-    dtu = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
-    dtrho = -(f(t - r) * (1 - pow(t - r, 2) / pow(W, 2)) -
-              f(t + r) * (1 - pow(t + r, 2) / pow(W, 2))) /
-            (pow(W, 2) * r);
-  }
+template <typename T>
+static inline auto sw_Dy(T A, T kx, T ky, T kz, T t, T x, T y,
+                         T z) noexcept -> T {
+  using std::sqrt, std::sin, std::cos;
+  const auto pi{acos(T{-1})};
+  const auto omega{sqrt(kx * kx + ky * ky + kz * kz)};
+  return 2 * A * ky * pi * cos(2 * omega * pi * t) * sin(2 * kx * pi * x) *
+         cos(2 * ky * pi * y) * sin(2 * kz * pi * z);
+}
+
+template <typename T>
+static inline auto sw_Dz(T A, T kx, T ky, T kz, T t, T x, T y,
+                         T z) noexcept -> T {
+  using std::sqrt, std::sin, std::cos;
+  const auto pi{acos(T{-1})};
+  const auto omega{sqrt(kx * kx + ky * ky + kz * kz)};
+  return 2 * A * kz * pi * cos(2 * omega * pi * t) * sin(2 * kx * pi * x) *
+         sin(2 * ky * pi * y) * cos(2 * kz * pi * z);
+}
+
+// Finite difference helpers
+enum class fd_dir : std::size_t { x = 0, y = 1, z = 2 };
+
+template <fd_dir direction, typename T>
+static inline auto fd_c_1_4(const Loop::PointDesc &p,
+                            const Loop::GF3D2<T> &gf) noexcept -> T {
+  // (1*f[i-2]-8*f[i-1]+8*f[i+1]-1*f[i+2])
+  constexpr auto d{static_cast<size_t>(direction)};
+  const auto num{gf(p.I - 2 * p.DI[d]) - 8.0 * gf(p.I - 1 * p.DI[d]) +
+                 8.0 * gf(p.I + 1 * p.DI[d]) - 1.0 * gf(p.I + 2 * p.DI[d])};
+  const auto den{1.0 / (12.0 * p.DX[d])};
+  return den * num;
 }
 
 extern "C" void TestRKAB_Initial(CCTK_ARGUMENTS) {
@@ -85,19 +92,29 @@ extern "C" void TestRKAB_Initial(CCTK_ARGUMENTS) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
-                        standing_wave_kz, cctk_time, p.x, p.y, p.z, u(p.I),
-                        rho(p.I));
+          const auto t{cctk_time};
+          const auto t_pre{t - cctk_delta_time};
+
+          const auto A{amplitude};
+          const auto kx{standing_wave_kx};
+          const auto ky{standing_wave_ky};
+          const auto kz{standing_wave_kz};
+
+          phi(p.I) = sw_phi(A, kx, ky, kz, t, p.x, p.y, p.z);
+          Pi(p.I) = sw_Pi(A, kx, ky, kz, t, p.x, p.y, p.z);
+          Dx(p.I) = sw_Dx(A, kx, ky, kz, t, p.x, p.y, p.z);
+          Dy(p.I) = sw_Dy(A, kx, ky, kz, t, p.x, p.y, p.z);
+          Dz(p.I) = sw_Dz(A, kx, ky, kz, t, p.x, p.y, p.z);
+
+          phi_pre(p.I) = sw_phi(A, kx, ky, kz, t_pre, p.x, p.y, p.z);
+          Pi_pre(p.I) = sw_Pi(A, kx, ky, kz, t_pre, p.x, p.y, p.z);
+          Dx_pre(p.I) = sw_Dx(A, kx, ky, kz, t_pre, p.x, p.y, p.z);
+          Dy_pre(p.I) = sw_Dy(A, kx, ky, kz, t_pre, p.x, p.y, p.z);
+          Dz_pre(p.I) = sw_Dz(A, kx, ky, kz, t_pre, p.x, p.y, p.z);
         });
 
   } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
-    grid.loop_int_device<0, 0, 0>(
-        grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u(p.I),
-                   rho(p.I));
-        });
-
+    CCTK_ERROR("Gaussian initial data not implemented yet");
   } else {
     CCTK_ERROR("Unknown initial condition");
   }
@@ -110,17 +127,12 @@ extern "C" void TestRKAB_RHS(CCTK_ARGUMENTS) {
   grid.loop_int_device<0, 0, 0>(
       grid.nghostzones,
       [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-        using std::pow;
-        Arith::vect<CCTK_REAL, dim> ddu;
-        for (int d = 0; d < dim; ++d) {
-          // ddu[d] = (u(p.I - p.DI[d]) - 2 * u(p.I) + u(p.I + p.DI[d])) /
-          //          pow(p.DX[d], 2);
-          ddu[d] = (-(u(p.I - 2 * p.DI[d]) + u(p.I + 2 * p.DI[d])) +
-                    16 * (u(p.I - p.DI[d]) + u(p.I + p.DI[d])) - 30 * u(p.I)) /
-                   (12 * pow(p.DX[d], 2));
-        }
-        u_rhs(p.I) = rho(p.I);
-        rho_rhs(p.I) = ddu[0] + ddu[1] + ddu[2];
+        phi_rhs(p.I) = Pi(p.I);
+        Pi_rhs(p.I) = fd_c_1_4<fd_dir::x>(p, Dx) + fd_c_1_4<fd_dir::y>(p, Dx) +
+                      fd_c_1_4<fd_dir::z>(p, Dz);
+        Dx_rhs(p.I) = fd_c_1_4<fd_dir::x>(p, Pi);
+        Dy_rhs(p.I) = fd_c_1_4<fd_dir::y>(p, Pi);
+        Dz_rhs(p.I) = fd_c_1_4<fd_dir::z>(p, Pi);
       });
 }
 
@@ -137,25 +149,36 @@ extern "C" void TestRKAB_Error(CCTK_ARGUMENTS) {
     grid.loop_int_device<0, 0, 0>(
         grid.nghostzones,
         [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          CCTK_REAL u0, rho0;
-          standing_wave(amplitude, standing_wave_kx, standing_wave_ky,
-                        standing_wave_kz, cctk_time, p.x, p.y, p.z, u0, rho0);
-          u_err(p.I) = u(p.I) - u0;
-          rho_err(p.I) = rho(p.I) - rho0;
+          using std::fabs;
+
+          const auto t{cctk_time};
+
+          const auto A{amplitude};
+          const auto kx{standing_wave_kx};
+          const auto ky{standing_wave_ky};
+          const auto kz{standing_wave_kz};
+
+          const auto expected_phi{sw_phi(A, kx, ky, kz, t, p.x, p.y, p.z)};
+          const auto expected_Pi{sw_Pi(A, kx, ky, kz, t, p.x, p.y, p.z)};
+          const auto expected_Dx{sw_Dx(A, kx, ky, kz, t, p.x, p.y, p.z)};
+          const auto expected_Dy{sw_Dy(A, kx, ky, kz, t, p.x, p.y, p.z)};
+          const auto expected_Dz{sw_Dz(A, kx, ky, kz, t, p.x, p.y, p.z)};
+
+          const auto actual_phi{phi(p.I)};
+          const auto actual_Pi{Pi(p.I)};
+          const auto actual_Dx{Dx(p.I)};
+          const auto actual_Dy{Dy(p.I)};
+          const auto actual_Dz{Dz(p.I)};
+
+          phi_err(p.I) = fabs(expected_phi - actual_phi);
+          Pi_err(p.I) = fabs(expected_Pi - actual_Pi);
+          Dx_err(p.I) = fabs(expected_Dx - actual_Dx);
+          Dy_err(p.I) = fabs(expected_Dy - actual_Dy);
+          Dz_err(p.I) = fabs(expected_Dz - actual_Dz);
         });
 
   } else if (CCTK_EQUALS(initial_condition, "Gaussian")) {
-
-    grid.loop_int_device<0, 0, 0>(
-        grid.nghostzones,
-        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-          CCTK_REAL u0, rho0;
-          gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u0,
-                   rho0);
-          u_err(p.I) = u(p.I) - u0;
-          rho_err(p.I) = rho(p.I) - rho0;
-        });
-
+    CCTK_VERROR("Gaussian error not implemented yet");
   } else {
     CCTK_ERROR("Unknown initial condition");
   }
