50d2b7dd1b5a5a333694fcda05677a8fc5808a14
Roland Haas <rhaas@illinois.edu>
2024-11-07 13:53:56 -0600

    Loop: explicitly create function args to avoid compiler issue

--- Loop/src/loop.hxx
+++ Loop/src/loop.hxx
@@ -187,8 +187,9 @@ public:
             // Outward boundary normal (if on outermost interior point), else 0
             const vect<int, dim> BI =
                 vect<int, dim>(I == bnd_max - 1) - vect<int, dim>(I == bnd_min);
+            const vect<bool, dim> CI1{CI, CJ, CK};
             const PointDesc p =
-                point_desc({CI, CJ, CK}, I, iter, NI, I0, BI, bnd_min, bnd_max,
+                point_desc(CI1, I, iter, NI, I0, BI, bnd_min, bnd_max,
                            loop_min, loop_max);
             f(p);
           }
