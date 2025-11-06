020189414cce2781e32e6f2484747b027136d9c2
Erik Schnetter <schnetter@gmail.com>
2025-11-06 14:35:43 -0500

    CarpetX: Allow per-group prolongation operators and orders

--- CarpetX/src/prolongate_3d_rf2_impl_cons.cxx
+++ CarpetX/src/prolongate_3d_rf2_impl_cons.cxx
@@ -4,38 +4,64 @@ namespace CarpetX {
 
 // Conservative interpolation
 
-prolongate_3d_rf2<VC, VC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<VC, VC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c000_o0;
-prolongate_3d_rf2<VC, VC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<VC, VC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c001_o0;
-prolongate_3d_rf2<VC, CC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<VC, CC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c010_o0;
-prolongate_3d_rf2<VC, CC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<VC, CC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c011_o0;
-prolongate_3d_rf2<CC, VC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<CC, VC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c100_o0;
-prolongate_3d_rf2<CC, VC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<CC, VC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c101_o0;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c110_o0;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 0, 0, 0, FB_NONE>
     prolongate_cons_3d_rf2_c111_o0;
 
-prolongate_3d_rf2<VC, VC, VC, CONS, CONS, CONS, 1, 1, 1, FB_NONE>
+static prolongate_3d_rf2<VC, VC, VC, CONS, CONS, CONS, 1, 1, 1, FB_NONE>
     prolongate_cons_3d_rf2_c000_o1;
-prolongate_3d_rf2<VC, VC, CC, CONS, CONS, CONS, 1, 1, 2, FB_NONE>
+static prolongate_3d_rf2<VC, VC, CC, CONS, CONS, CONS, 1, 1, 2, FB_NONE>
     prolongate_cons_3d_rf2_c001_o1;
-prolongate_3d_rf2<VC, CC, VC, CONS, CONS, CONS, 1, 2, 1, FB_NONE>
+static prolongate_3d_rf2<VC, CC, VC, CONS, CONS, CONS, 1, 2, 1, FB_NONE>
     prolongate_cons_3d_rf2_c010_o1;
-prolongate_3d_rf2<VC, CC, CC, CONS, CONS, CONS, 1, 2, 2, FB_NONE>
+static prolongate_3d_rf2<VC, CC, CC, CONS, CONS, CONS, 1, 2, 2, FB_NONE>
     prolongate_cons_3d_rf2_c011_o1;
-prolongate_3d_rf2<CC, VC, VC, CONS, CONS, CONS, 2, 1, 1, FB_NONE>
+static prolongate_3d_rf2<CC, VC, VC, CONS, CONS, CONS, 2, 1, 1, FB_NONE>
     prolongate_cons_3d_rf2_c100_o1;
-prolongate_3d_rf2<CC, VC, CC, CONS, CONS, CONS, 2, 1, 2, FB_NONE>
+static prolongate_3d_rf2<CC, VC, CC, CONS, CONS, CONS, 2, 1, 2, FB_NONE>
     prolongate_cons_3d_rf2_c101_o1;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, CONS, 2, 2, 1, FB_NONE>
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, CONS, 2, 2, 1, FB_NONE>
     prolongate_cons_3d_rf2_c110_o1;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 2, 2, 2, FB_NONE>
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 2, 2, 2, FB_NONE>
     prolongate_cons_3d_rf2_c111_o1;
+
+const std::map<int, std::array<amrex::Interpolater *, 8> >
+    prolongate_cons_3d_rf2{
+        {0,
+         {
+             &prolongate_cons_3d_rf2_c000_o0,
+             &prolongate_cons_3d_rf2_c001_o0,
+             &prolongate_cons_3d_rf2_c010_o0,
+             &prolongate_cons_3d_rf2_c011_o0,
+             &prolongate_cons_3d_rf2_c100_o0,
+             &prolongate_cons_3d_rf2_c101_o0,
+             &prolongate_cons_3d_rf2_c110_o0,
+             &prolongate_cons_3d_rf2_c111_o0,
+         }},
+        {1,
+         {
+             &prolongate_cons_3d_rf2_c000_o1,
+             &prolongate_cons_3d_rf2_c001_o1,
+             &prolongate_cons_3d_rf2_c010_o1,
+             &prolongate_cons_3d_rf2_c011_o1,
+             &prolongate_cons_3d_rf2_c100_o1,
+             &prolongate_cons_3d_rf2_c101_o1,
+             &prolongate_cons_3d_rf2_c110_o1,
+             &prolongate_cons_3d_rf2_c111_o1,
+         }},
+    };
 
 } // namespace CarpetX
--- CarpetX/src/prolongate_3d_rf2_impl_poly_cons3lfb.cxx
+++ CarpetX/src/prolongate_3d_rf2_impl_poly_cons3lfb.cxx
@@ -6,74 +6,116 @@ namespace CarpetX {
 // with 3rd order accuracy and a linear fallback in cell centred
 // directions
 
-prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c000_o1;
-prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c001_o1;
-prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c010_o1;
-prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c011_o1;
-prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c100_o1;
-prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c101_o1;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c110_o1;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 1, 1, 1, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 1, 1, 1, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c111_o1;
 
-prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c000_o3;
-prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c001_o3;
-prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c010_o3;
-prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c011_o3;
-prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c100_o3;
-prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c101_o3;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c110_o3;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c111_o3;
 
-prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 5, 5, 5, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 5, 5, 5, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c000_o5;
-prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 5, 5, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 5, 5, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c001_o5;
-prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 5, 3, 5, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 5, 3, 5, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c010_o5;
-prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 5, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 5, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c011_o5;
-prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 5, 5, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 5, 5, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c100_o5;
-prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 5, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 5, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c101_o5;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 5, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 5, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c110_o5;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>
     prolongate_poly_cons3lfb_3d_rf2_c111_o5;
 
 #if 0
-prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 7, 7, 7, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c000_o7;
-prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 7, 7, 3, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c001_o7;
-prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 7, 3, 7, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c010_o7;
-prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 7, 3, 3, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c011_o7;
-prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 7, 7, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c100_o7;
-prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 7, 3, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c101_o7;
-prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 7, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c110_o7;
-prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>
-    prolongate_poly_cons3lfb_3d_rf2_c111_o7;
+static prolongate_3d_rf2<VC, VC, VC, POLY, POLY, POLY, 7, 7, 7, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c000_o7;
+static prolongate_3d_rf2<VC, VC, CC, POLY, POLY, CONS, 7, 7, 3, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c001_o7;
+static prolongate_3d_rf2<VC, CC, VC, POLY, CONS, POLY, 7, 3, 7, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c010_o7;
+static prolongate_3d_rf2<VC, CC, CC, POLY, CONS, CONS, 7, 3, 3, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c011_o7;
+static prolongate_3d_rf2<CC, VC, VC, CONS, POLY, POLY, 3, 7, 7, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c100_o7;
+static prolongate_3d_rf2<CC, VC, CC, CONS, POLY, CONS, 3, 7, 3, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c101_o7;
+static prolongate_3d_rf2<CC, CC, VC, CONS, CONS, POLY, 3, 3, 7, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c110_o7;
+static prolongate_3d_rf2<CC, CC, CC, CONS, CONS, CONS, 3, 3, 3, FB_LINEAR>    prolongate_poly_cons3lfb_3d_rf2_c111_o7;
 #endif
+
+const std::map<int, std::array<amrex::Interpolater *, 8> >
+    prolongate_poly_cons3lfb_3d_rf2{
+        {1,
+         {
+             &prolongate_poly_cons3lfb_3d_rf2_c000_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c001_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c010_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c011_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c100_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c101_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c110_o1,
+             &prolongate_poly_cons3lfb_3d_rf2_c111_o1,
+         }},
+        {3,
+         {
+             &prolongate_poly_cons3lfb_3d_rf2_c000_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c001_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c010_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c011_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c100_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c101_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c110_o3,
+             &prolongate_poly_cons3lfb_3d_rf2_c111_o3,
+         }},
+        {5,
+         {
+             &prolongate_poly_cons3lfb_3d_rf2_c000_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c001_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c010_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c011_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c100_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c101_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c110_o5,
+             &prolongate_poly_cons3lfb_3d_rf2_c111_o5,
+         }},
+#if 0
+        {7,
+         {
+             &prolongate_poly_cons3lfb_3d_rf2_c000_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c001_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c010_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c011_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c100_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c101_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c110_o7,
+             &prolongate_poly_cons3lfb_3d_rf2_c111_o7,
+         }},
+#endif
+    };
 
 } // namespace CarpetX
