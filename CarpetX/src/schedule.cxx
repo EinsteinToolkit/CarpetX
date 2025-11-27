cc75c00d47f2af2835b5c586a59aaf5948a75d97
Liwei Ji <jiliwei.phys@gmail.com>
2025-11-26 21:29:34 -0500

    CarpetX: beautify getting min_acitve_level

--- CarpetX/src/schedule.cxx
+++ CarpetX/src/schedule.cxx
@@ -1666,13 +1666,12 @@ int Evolve(tFleshConfig *config) {
       for (const auto &patchdata : ghext->patchdata) {
 
         int min_active_level = -1;
-        for (int min_level = patchdata.leveldata.size() - 1; min_level >= 0;
-             --min_level)
-          if (patchdata.leveldata.at(min_level).iteration == iteration) {
-            min_active_level = min_level;
-          } else {
+        for (int level = patchdata.leveldata.size() - 1; level >= 0; --level) {
+          if (patchdata.leveldata.at(level).iteration != iteration) {
             break;
           }
+          min_active_level = level;
+        }
         assert(min_active_level != -1);
 
         const int old_numlevels = patchdata.amrcore->finestLevel() + 1;
