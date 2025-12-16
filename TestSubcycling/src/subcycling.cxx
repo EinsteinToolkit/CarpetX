fdf580534bc946ecec75d0a43e78bcf3dbccee6c
Liwei Ji <jiliwei.phys@gmail.com>
2025-12-16 09:21:52 -0500

    TestSubcycling: apply boundary conditions setting initial data

--- TestSubcycling/src/subcycling.cxx
+++ TestSubcycling/src/subcycling.cxx
@@ -10,7 +10,7 @@ void TestSubcycling_Init(CCTK_ARGUMENTS)
   DECLARE_CCTK_ARGUMENTSX_TestSubcycling_Init;
 
   CCTK_INFO("Initializing grid function");
-  grid.loop_all<0,0,0>(grid.nghostzones, [=](const Loop::PointDesc &pt) {
+  grid.loop_int<0,0,0>(grid.nghostzones, [=](const Loop::PointDesc &pt) {
     CCTK_REAL canary = 100 + 10 * cctk_iteration + 1 * cctk_level;
     iteration(pt.I) = canary;
   });
