aba19fd4114e662c670c02fe3c18f438f0595834
Liwei Ji <jiliwei.phys@gmail.com>
2026-02-26 09:22:06 -0500

    CarpetX: rename FullSync to SyncEvolvedGFs

--- CarpetX/src/schedule.cxx
+++ CarpetX/src/schedule.cxx
@@ -83,7 +83,7 @@ std::optional<active_levels_t> active_levels;
 void Reflux(const cGH *cctkGH, int level);
 void Restrict(const cGH *cctkGH, int level, const std::vector<int> &groups);
 void Restrict(const cGH *cctkGH, int level);
-void FullSync(const cGH *cctkGH);
+void SyncEvolvedGFs(const cGH *cctkGH);
 
 namespace {
 // Convert a (direction, face) pair to an AMReX Orientation
@@ -1389,7 +1389,7 @@ int Initialise(tFleshConfig *config) {
         Restrict(cctkGH, leveldata.level);
     });
     // Prolongation
-    FullSync(cctkGH);
+    SyncEvolvedGFs(cctkGH);
     CCTK_Traverse(cctkGH, "CCTK_POSTRESTRICT");
   }
 
@@ -1873,7 +1873,7 @@ int Evolve(tFleshConfig *config) {
         active_levels->loop_fine_to_coarse(
             [&](const auto &leveldata) { Restrict(cctkGH, leveldata.level); });
         // Prolongation
-        FullSync(cctkGH);
+        SyncEvolvedGFs(cctkGH);
         CCTK_Traverse(cctkGH, "CCTK_POSTRESTRICT");
       }
 
@@ -2914,7 +2914,7 @@ void Restrict(const cGH *cctkGH, int level) {
   Restrict(cctkGH, level, groups);
 }
 
-void FullSync(const cGH *cctkGH) {
+void SyncEvolvedGFs(const cGH *cctkGH) {
   const int numgroups = CCTK_NumGroups();
   vector<int> groups;
   groups.reserve(numgroups);
